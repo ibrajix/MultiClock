89d4352240884cf4cabadcde6c588f6e7f5aa1df
ibrajix <ibrajix@gmail.com>
2022-01-14 12:36:02 +0100

    Added column info to Alarm.kt entity

--- app/src/main/java/com/ibrajix/multiclock/database/Alarm.kt
+++ app/src/main/java/com/ibrajix/multiclock/database/Alarm.kt
@@ -1,13 +1,18 @@
 package com.ibrajix.multiclock.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "alarm_table")
 data class Alarm(
   @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "id")
   val id: Int,
+  @ColumnInfo(name = "time")
   val time: String,
+  @ColumnInfo(name = "status")
   val status: Boolean,
+  @ColumnInfo(name = "vibrate")
   val vibrate: Boolean
 )
